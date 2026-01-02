a97712c4e0bd584cad3bb4a2a84850f491adef93
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-01-02 04:47:20 -0300

    se agregan variables de entorno yarn add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsuariosModule } from './usuarios/usuarios.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',      // o el nombre del contenedor
-      port: 5432,
-      username: 'findpet_user',
-      password: 'findpet_password',
-      database: 'findpet_db',
-      autoLoadEntities: true,
-      synchronize: false,     // MUY importante en producción
-    }),
-    UsuariosModule,
-
+    ConfigModule.forRoot()
   ],
 })
 export class AppModule {}
