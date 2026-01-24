c2aa7d6bcf409cf455ef75a7cdc02427268cac64
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-01-23 22:41:33 -0300

    fix: reorder TypeOrmModule configuration for clarity and consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,21 +10,29 @@ import { PublicacionesModule } from './publicaciones/publicaciones.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       port: Number(process.env.DB_PORT),
-      database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: process.env.NODE_ENV === 'production',
+      extra: {
+        ssl:
+          process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+      },
     }),
-    
-    UsuariosModule, 
-    
+
+
+    UsuariosModule,
+
     SeedModule, AuthModule, MascotasModule, PublicacionesModule
   ],
 })
-export class AppModule {}
+export class AppModule { }
