ce39b941247a89c707815d77ac9dfe6d2089a966
Elizabeth Pincheira <elizabeth.pincheira.caro@gmail.com>
2026-01-23 23:10:32 -0300

    fix: streamline port assignment and logging in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -29,9 +29,11 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
-}
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-
+  console.log(`🚀 App corriendo en puerto ${port}`);
 }
 bootstrap();
+
+}
